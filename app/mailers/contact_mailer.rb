af4cb646e2d35d4e960bbb5cee4c4cfcc1a65360
Timothy Boyle <jboyle85@me.com>
2015-11-18 02:38:35 +0000

    Fixed mailer html file name

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
     default to: 'jboyle85@me.com'
     
-    def contact_email(name,email, body)
+    def contact_email(name, email, body)
         @name = name
         @email = email
         @body = body
