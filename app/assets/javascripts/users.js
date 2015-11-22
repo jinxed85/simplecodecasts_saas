75248f0afd726bd99f645ff13c31cb223752a71a
Timothy Boyle <jboyle85@me.com>
2015-11-22 05:21:21 +0000

    Fixed files to get stripe to work

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -9,10 +9,10 @@ $(document).ready(function() {
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
-   
+
     if (!error) {
       // Get the Stripe token:
-      Stripe.createToken({
+      Stripe.card.createToken({
         number: ccNum,
         cvc: cvcNum,
         exp_month: expMonth,
@@ -25,10 +25,13 @@ $(document).ready(function() {
   function stripeResponseHandler(status, response) {
     // Get a reference to the form:
     var f = $("#new_user");
+
     // Get the token from the response:
     var token = response.id;
+
     // Add the token to the form:
     f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
+
     // Submit the form:
     f.get(0).submit(); 
   }
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
    
    def create
-      super do [resource]
+      super do |resource|
         if params[:plan]
             resource.plan_id = params[:plan]
             if resource.plan_id == 2
